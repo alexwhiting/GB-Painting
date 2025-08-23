a1e6dd5430f0551b837608907c75526b03414884
Alex Whiting <alexanderwhiting@icloud.com>
2025-08-23 15:11:06 -0300

    Creatted function to generate html for areas of operation

---
+++ scripts/index.js
@@ -0,0 +1,93 @@
+import { handleModalForm } from './modalForm.js';
+
+handleModalForm();
+
+// Script for scroll animations
+document.addEventListener('DOMContentLoaded', function () {
+  const animateOnScroll = function () {
+    const elements = document.querySelectorAll('.scroll-animate');
+    const windowHeight = window.innerHeight;
+
+    elements.forEach(element => {
+      const elementPosition = element.getBoundingClientRect().top;
+
+      if (elementPosition < windowHeight * 0.85) {
+        element.classList.add('active');
+      }
+    });
+  };
+
+
+  animateOnScroll();
+  window.addEventListener('scroll', animateOnScroll);
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      const headerHeight = document.querySelector('header').offsetHeight;
+      const targetPosition = targetElement.offsetTop - headerHeight - 20;
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+const areas = [{
+  location: 'Annapolis Valley',
+  locationSpecifics: [
+    'Wolfville',
+    'New Minas',
+    'Kentville',
+    'And more!'
+  ]
+}, {
+  location: 'Halifax and Surrounding Areas',
+  locationSpecifics: [
+    'Bedford',
+    'Sackville',
+    'Tantallon',
+    'And more!'
+  ]
+}, {
+  location: 'Dartmouth',
+  locationSpecifics: [
+    'Cole Harbour',
+    'Eastern Passage',
+    'Portland',
+    'And more!'
+  ]
+}];
+
+function areasOfOperationHTML() {
+  let html = '';
+
+  areas.forEach((area) => {
+    let listItemsHTML = '';
+    html +=
+      `
+      <div class="col-md-4 mb-4">
+        <div class="expertise-card">
+          <div class="expertise-icon">
+            <i class="fa-solid fa-apple-whole"></i>
+          </div>
+          <h3 data-location>
+            ${area.location}
+          </h3>
+          <ul class="expertise-list" data-location-specifics>
+            ${area.locationSpecifics.map(item => `<li>${item}</li>`).join('')}
+          </ul>
+        </div>
+      </div>
+    `;
+  });
+
+  document.querySelector('.js-areas-of-operation').innerHTML = html;
+}
+
+areasOfOperationHTML();
---
+++ scripts/portfolio.js
@@ -0,0 +1,3 @@
+import { handleModalForm } from './modalForm.js';
+
+handleModalForm();
