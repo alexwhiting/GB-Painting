91fc28b92c69984dffa1d0f5e2fb15cb3cf01681
Alex Whiting <alexanderwhiting@icloud.com>
2025-09-22 18:42:30 -0300

    delete all old files

--- scripts/services.js
+++
@@ -1,9 +0,0 @@
-import { handleModalForm } from './modalForm.js';
-import { generateHeader, generateMobileHeader, toggleMobileMenu, generateFooter, generateMobileFooter } from './general.js';
-
-generateHeader();
-generateMobileHeader();
-toggleMobileMenu();
-handleModalForm();
-generateFooter();
-generateMobileFooter();
